d7628cef76350b7930fa7a8c81e199abdf2c8be3
lsenku <sgulyaeva1979@gmail.com>
2024-05-16 20:42:42 +0400

    Add a lot of everything

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,13 +1,57 @@
 ﻿#include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
-const int RangeField = 50;
+bool gameOver;
+const int width = 118;
+const int hieght = 33;
+int x, y, BlockX, BlockY, plane, monster, shot, score;
+enum  eDirection { STOP = 0, LEFT, REIGHT, UP, DOWN };
+eDirection dir;
 
-int main() {
-	
-
-
-	return 0;
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = hieght / 2;
+    BlockX = rand() % width;
+    BlockY = rand() % hieght;
+    score = 0;
 }
+
+void Draw() {
+    system("cls");
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+    cout << endl;
+    for (int i = 0; i < hieght; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0 || j == width - 1)
+                cout << "#";
+            cout << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+    cout << endl;
+}
+void Input() {
+
+}
+void Logic() {
+
+}
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+    }
+    return 0;
+}
+
+
 
